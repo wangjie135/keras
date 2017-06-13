5ab7c9b133b50afc9e00002f1526e1aedadada66
jjallaire <jj@rstudio.com>
2017-06-13 11:02:11 -0400

    make layer a public member; default impls

--- R/layers-custom.R
+++ R/layers-custom.R
@@ -7,30 +7,32 @@ KerasLayer <- R6Class("KerasLayer",
   
   public = list(
     
-    # TODO: standard layer args
     initialize = function() {
       
       # import layer utility module
       python_path <- system.file("python", package = "keras")
       tools <- import_from_path("kerastools", path = python_path)
       
-      private$py_layer <- tools$layer$RLayer(
+      # create the layer
+      self$layer <- tools$layer$RLayer(
         function (input_shape) self$build(input_shape),
         function (x) self$call(x),
         function (input_shape) self$compute_output_shape(input_shape)
       )
     },
     
+    layer = NULL,
+    
     build = function(input_shape) {
       
     },
     
     call = function(x) {
-      
+      x
     },
-    
+  
     compute_output_shape = function(input_shape) {
-      
+      input_shape
     }
   ),
   
@@ -38,3 +40,5 @@ KerasLayer <- R6Class("KerasLayer",
     py_layer = NULL
   )
 )
+
+
